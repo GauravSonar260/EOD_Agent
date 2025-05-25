32e35d26f109deb40d3d21e0eb7f8134e46573a6
gaurav4445 <116260842+gaurav4445@users.noreply.github.com>
2025-05-25 14:12:58 +0530

    Update EOD_REPORT_GENERATOR.py
    Something minor changes

--- EOD_REPORT_GENERATOR.py
+++ EOD_REPORT_GENERATOR.py
@@ -11,16 +11,16 @@ import base64
 st.set_page_config(page_title="EOD Banking Report", layout="wide")
 
 BRANCH_MANAGER_EMAILS = {
-    "Mumbai": "patilsakshee41@gmail.com",
-    "Delhi": "patilsakshee41@gmail.com",
-    "Bangalore": "patilsakshee41@gmail.com",
-    "Default": "patilsakshee41@gmail"
+    "Mumbai": "recievermail",
+    "Delhi": "recievermail",
+    "Bangalore": "recievermail",
+    "Default": "recievermail"
 }
 
 SMTP_SERVER = "smtp.gmail.com"
 SMTP_PORT = 587
-EMAIL_USER = "patilsakshee41@gmail.com"
-EMAIL_PASSWORD = "bkfx ssoj rvdy aiid"  # Keep this safe!
+EMAIL_USER = "sender mail "
+EMAIL_PASSWORD = "app_pass"  # Keep this safe!
 
 LOAN_TYPES = ['Gold Loan', 'Home Loan', 'Education Loan']
 
